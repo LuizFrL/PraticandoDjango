0a879b0d2135fa90a02952723a81220c6295360a
Luiz Fernando <luizfernandorole@gmail.com>
2020-07-13 00:19:55 -0300

    Adicionando itens ao portifolio

--- apps/Portifolio/models.py
+++ apps/Portifolio/models.py
@@ -1,5 +1,16 @@
+from datetime import datetime
+
 from django.contrib.auth.models import User
 from django.db import models
+
+
+
+def upload_to_item(instance, filename):
+    return 'fotos/portifolio/item/{}/{}'.format(instance.user.id, filename)
+
+
+def upload_to_portifolio(instance, filename):
+    return 'fotos/portifolio/foto_perfil/{}/{}'.format(instance.user.id, filename)
 
 
 # Create your models here.
@@ -7,4 +18,12 @@ class Portfolio(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=90)
     descricao = models.TextField()
-    imagem = models.FileField(upload_to='fotos/portifolio/%d/%m/%Y/', blank=True)
+    imagem = models.FileField(upload_to=upload_to_portifolio, blank=True)
+
+
+class PortifolioItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=40)
+    foto = models.ImageField(upload_to=upload_to_item, blank=True)
+    descricao = models.TextField(max_length=200)
+    data = models.DateField(default=datetime.today())
--- apps/Portifolio/views.py
+++ apps/Portifolio/views.py
@@ -3,17 +3,35 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.shortcuts import render, redirect
 
+from apps.Portifolio.models import PortifolioItem
 from apps.Usuarios.models import Profile
+
+
+def create_item(request):
+    form = request.POST
+    print(form)
+    portifolio_item = PortifolioItem.objects.create(
+        user_id=request.user.id,
+        titulo=form['titulo'],
+        foto=request.FILES['foto'],
+        descricao=form['descricao']
+    )
+    portifolio_item.save()
 
 
 def portifolio_pessoal(request):
     if request.user.is_authenticated:
+        if request.method == 'POST':
+            create_item(request)
         data = {
-            'perfil': get_object_or_404(Profile, pk=request.user.id)
+            'perfil': get_object_or_404(Profile, pk=request.user.id),
+            'portifolios': PortifolioItem.objects.order_by('-data').filter(user_id=request.user.id)
         }
+        print(data)
         return render(request, 'portifolio/portifolio_pessoal.html', data)
     return redirect('cadastro')
 
 
 def preview(request):
     return render(request, 'portifolio/model_portifolio.html')
+
